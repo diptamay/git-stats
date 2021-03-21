e54bf166faec98efbffc277cef8e9eacf9b2daa0
Diptamay Sanyal <diptamay@gmail.com>
2021-03-21 19:19:35 -0400

    Updates

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 array1=( colossus collins )
+org='tumblr'
 for i in "${array1[@]}"
 do
-  echo "Generating stats for $i with token $1"
-	npm run app stats github tumblr X "$i" $1
+  echo
+  echo "Generating stats for ${org}/$i with token $1"
+	npm run app stats github "${org}" X "$i" $1
 done
